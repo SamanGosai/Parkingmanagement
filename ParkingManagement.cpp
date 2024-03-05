1dbd16e87d5ab29edcef0d01a6a1c6cdd5b4161d
SamanGosai <156528627+SamanGosai@users.noreply.github.com>
2024-03-05 11:23:18 +0545

    Update ParkingManagement.cpp

--- ParkingManagement.cpp
+++ ParkingManagement.cpp
@@ -3,16 +3,18 @@ using namespace std;
 class Parking_Spot {
 public:
     int Time, Price,Token;
-    string status;
+    string status,Name;
     Parking_Spot() :status("___"){}
 };
 class Queue {
 public:
+    int Time;
+    string Name;
     Queue* next;
 };
 int No_ofVehicle=0;
 Queue* head = NULL;
-Parking_Spot P_Arr[5][10],P_obj;
+Parking_Spot P_Arr[5][10];
 void Display_Parkingspot() {
     for (int i = 1; i <= 42; i++) { cout << "-"; }
     cout << endl << " ";
@@ -30,11 +32,9 @@ void Display_Parkingspot() {
     for (int i = 1; i <= 42; i++) { cout << "-"; }
     cout << endl << endl;
 }
-void Park() {
-    cout << "Enter for how many hours:  ";
-    cin >> P_obj.Time;
+void Park(int Time,string Name) {
     int row, clmn;
-    if (P_obj.Time >= 3) {
+    if (Time >= 3) {
         for (int i = 9; i >= 0; i--) {
             for (int j = 0; j < 5; j++) {
                 if (P_Arr[j][i].status == "___") {
@@ -61,10 +61,14 @@ void Park() {
         }
     }
     P_Arr[row][clmn].status = "|-|";
-    P_Arr[row][clmn].Time = P_obj.Time;
-    P_Arr[row][clmn].Price = P_obj.Time * 150;
+    P_Arr[row][clmn].Name = Name;
+    P_Arr[row][clmn].Time = Time;
+    if (Time == 1)
+        P_Arr[row][clmn].Price = 120;
+    else
+        P_Arr[row][clmn].Price = 120 + (Time - 1) * 90;
     P_Arr[row][clmn].Token = row * 10 + clmn;
-    cout << "Your Token is: " << P_Arr[row][clmn].Token << endl;
+    cout <<Name<<", Your Token is: " << P_Arr[row][clmn].Token << endl;
 }
 void Depart() {
     int token, row, clmn;
@@ -72,14 +76,17 @@ void Depart() {
     cin >> token;
     row = token / 10;
     clmn = token % 10;
+    cout << "Thanks For Using Our Service " << P_Arr[row][clmn].Name << endl;
     cout << "Your Total Price is:" << P_Arr[row][clmn].Price << endl;
     P_Arr[row][clmn].status = "___";
 }
-void Enqueue(){
+void Enqueue(int Time, string Name){
     cout << "Enqueud Sucessfully!!!" << endl;
     No_ofVehicle++;
     cout<<"There are "<< No_ofVehicle<<" Vehicles in Queue"<<endl;
     Queue *New_node = new Queue;
+    New_node->Name = Name;
+    New_node->Time = Time;
     if (head == NULL) {
         New_node->next = NULL;
         head = New_node;
@@ -98,6 +105,7 @@ void Dequeue() {
         temp2 = temp1->next;
         if (temp2 == NULL) {
             head = NULL;
+            Park(temp1->Time,temp1->Name);
             delete temp1;
         }
         else {
@@ -106,29 +114,53 @@ void Dequeue() {
                 temp2 = temp1->next;
             }
             temp1->next = NULL;
+            Park(temp2->Time, temp2->Name);
             delete temp2;
         }
-        Park();
     }
     No_ofVehicle--;
 }
 int main()
 {
-    int option;
+    int option,check,Time;
+    string Name;
     cout << "\t\t\t\tWELCOME TO PARKING MANAGEMENT CONSOLE!!!!!!!" << endl<<endl;
+    cout << "Prices: " << endl;
+    cout << "120 rs upto 1 hrs" << endl;
+    cout << "90 for every additional hrs" << endl;
     do {
-        cout << "Press 1 To Enque,2 To Dequeue,3 To Depart,0 To Exit Console: ";
+        cout << "Press 1 To Park,2 To Modify Queue,3 To Depart,4 To Display Parking,0 To Exit Console: ";
         cin >> option;
         switch (option) {
         case 1:
-            Enqueue();
+            cout << "Enter your Name: ";
+            cin >> Name;
+            cout << "Enter for how many hours:  ";
+            cin >> Time;
+            if (head == NULL)
+                Park(Time,Name);
+            else {
+                cout << "Enqueuing in queue!!" << endl;
+                Enqueue(Time, Name);
+            }
             break;
         case 2:
-            Dequeue();
-            Display_Parkingspot();
+            cout << "Enqueue(1) or dequeue(2): ";
+            cin >> check;
+            if (check == 1) {
+                cout << "Enter your Name: ";
+                cin >> Name;
+                cout << "Enter for how many hours:  ";
+                cin >> Time;
+                Enqueue(Time, Name);
+            }
+            else
+                Dequeue();
             break;
         case 3:
             Depart();
+            break;
+        case 4:
             Display_Parkingspot();
             break;
         case 0:
